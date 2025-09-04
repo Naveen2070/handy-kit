603fcc8b61aacda49d8e5e7de91bb96e9c9a11d1
Naveen2070 <104181086+Naveen2070@users.noreply.github.com>
2025-09-04 15:10:24 +0530

    vitest config added

---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    reporters: ["verbose"],
+    coverage: {
+      enabled: true,
+      cleanOnRerun: true,
+      provider: "v8",
+      reporter: ["html", "json", "text"],
+      reportOnFailure: true,
+      reportsDirectory: "coverage",
+    },
+    environment: "node",
+    ui: true,
+    bail: 5,
+    watch: true,
+  },
+  cacheDir: "./node_modules/.vitest",
+});
